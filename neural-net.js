65c831ff08ed085eb86e836f5a4b6dac93da1ec1
Sergei Liddell <liddell.s@pm.me>
2021-03-15 21:24:48 -0600

    Creates neural net for team winning probability

--- neural-net.js
+++ neural-net.js
@@ -1,6 +1,7 @@
 const brain = require('brain.js');
 
 const numbers = new brain.NeuralNetwork();
+const teamWinner = new brain.NeuralNetwork();
 
 numbers.train([
   { input: [0, 0, 0], output: [0] },
@@ -10,6 +11,19 @@ numbers.train([
   { input: [1, 1, 1], output: [1] }
 ])
 
-const result = numbers.run([1, 0, 0])
+teamWinner.train([
+  { input: [1, 2], output: [1] }, // team 2 won
+  { input: [1, 3], output: [1] }, // team 3 won
+  { input: [2, 3], output: [0] }, // team 2 won
+  { input: [1, 2], output: [1] }, // team 2 won
+  { input: [1, 3], output: [1] } // team 3 won, team 1 has never won
+])
 
-console.log(`Prob: ${result}`)
+
+const result = numbers.run([1, 0, 0]);
+const winningTeamProbably = teamWinner.run([2, 1]) // 1 not win
+const winningTeamProbably2 = teamWinner.run([1, 2]) // 2 win
+
+console.log(`numbers Prob: ${result}`)
+console.log(`winningTeamProbably Prob: ${winningTeamProbably}`)
+console.log(`winningTeamProbably2 Prob: ${winningTeamProbably2}`)
